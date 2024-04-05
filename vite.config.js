c6c6b554132391e3476c2d0aa6ad29f492911bb8
priy-am <priyam2101753@gmail.com>
2024-04-06 00:26:00 +0530

    small change

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/YoutubeClone",
   plugins: [react()],
-})
+  build: {
+    // generate .vite/manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: '/path/to/main.js',
+    },
+  },
+});
